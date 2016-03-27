943f046bf8333ca193cacca20f778c73a5f23a21
Alina Vlasova <alina13578@yandex.ru>
2016-03-27 18:19:16 +0300

    исправила некоторые замечания

--- sources/Core/sea_battle_api.h
+++ sources/Core/sea_battle_api.h
@@ -5,22 +5,35 @@ class Sea_Battle_API{
 
 public:
 
-    /// Начать новую игру
-    void start_new_game();
-    /// Продолжить сохраненную игру
-    void continue_saved_game();
-    /// Сохранить игру
-    void save_game();
-    /// Разместить корабли
-    void locate_ships();
-    /// Сделать ход на указанные игроком координаты
-    void make_move();
-    /// Пометить клетку крестиком, если игрок попал в корабль противника
-    void place_cross();
-    /// Пометить клетку точкой, если игрок не попал
-    void place_point();
-    /// Получить текущий статус игры
-    void get_status();
+    /**
+     * @brief Начать новую игру
+     */
+    virtual void start_new_game()=0;
+    /**
+     * @brief Продолжить сохраненную игру
+     */
+    virtual void continue_saved_game()=0;
+    /**
+     * @brief Сохранить игру
+     */
+    virtual void save_game()=0;
+    /**
+     * @brief Разместить корабли
+     */
+    virtual void locate_ships()=0;
+    /**
+     * @brief Сделать ход на указанные игроком координаты
+     */
+    virtual void make_move()=0;
+    /**
+     * @brief Пометить клетку крестиком, если игрок попал в корабль противника
+     */
+    virtual void place_cross()=0;
+    /**
+     * @brief Пометить клетку точкой, если игрок не попал
+     */
+    virtual void place_point()=0;
+
 };
 
 #endif // SEA_BATTLE_API_H
